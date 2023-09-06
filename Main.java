110a5c768191683bf7bcb61ddff3de0c35d06e4b
yergozhin <101567490+yergozhin@users.noreply.github.com>
2023-09-06 20:50:33 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Main extends JFrame {
+    public static void main(String[] args) {
+        PacmanGameWindow pacmanGameWindow = new PacmanGameWindow();
+        pacmanGameWindow.gameWindow(500,500);
+    }
+}
